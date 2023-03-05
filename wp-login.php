6b6c244d22ced7a72cbdd446e617f6aa6bb18b36
Gerges Alfy <90783065+gergesalfy@users.noreply.github.com>
2023-03-05 14:53:04 +0200

    ESO

---
+++ wp-login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://gergesalfy.github.io/ESO/login/?http://simply_static_page=24963">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://gergesalfy.github.io/ESO/login/?http://simply_static_page=24963";
+		</script>
+
+		<p>You are being redirected to <a href="https://gergesalfy.github.io/ESO/login/?http://simply_static_page=24963">https://gergesalfy.github.io/ESO/login/?http://simply_static_page=24963</a></p>
+	</body>
+</html>
